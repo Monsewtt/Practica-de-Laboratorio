0b98728c734cae01e0b9da98429526b7aa5f66f0
miamo <miamo@LAPTOP-57SRBEA7>
2026-03-19 12:21:52 -0600

    Agregar archivos de proyecto.

---
+++ Practica de Laboratorio/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace Practica_de_Laboratorio
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnMostrar = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnMostrar
+            // 
+            this.btnMostrar.Location = new System.Drawing.Point(84, 169);
+            this.btnMostrar.Name = "btnMostrar";
+            this.btnMostrar.Size = new System.Drawing.Size(120, 35);
+            this.btnMostrar.TabIndex = 0;
+            this.btnMostrar.Text = "Mostrar";
+            this.btnMostrar.UseVisualStyleBackColor = true;
+            this.btnMostrar.Click += new System.EventHandler(this.btnMostrar_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(268, 52);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(497, 367);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(87, 228);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(116, 28);
+            this.btnGuardar.TabIndex = 2;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnMostrar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnMostrar;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnGuardar;
+    }
+}
+
---
+++ Practica de Laboratorio/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Data.SqlClient;
+namespace Practica_de_Laboratorio
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMostrar_Click(object sender, EventArgs e)
+        {
+            string miIP = "192.168.1.113";
+            string cadena = $@"Server={miIP}\SQLEXPRESS; Database=PracticaLab; User Id=sa; Password=123; TrustServerCertificate=True;";
+
+            using (SqlConnection conexion = new SqlConnection(cadena))
+            {
+                try
+                {
+                    conexion.Open();
+                    string query = "SELECT * FROM Ciudadanos";
+
+                    SqlDataAdapter adaptador = new SqlDataAdapter(query, conexion);
+                    DataTable dt = new DataTable();
+                    adaptador.Fill(dt);
+                    dataGridView1.DataSource = dt;
+
+                    MessageBox.Show("¡Datos cargados desde la otra PC!");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error de red: " + ex.Message);
+                }
+            }
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            // Usa la misma IP y credenciales que en tu botón de Mostrar
+            string miIP = "192.168.1.113";
+            string cadena = $@"Server={miIP}\SQLEXPRESS; Database=PracticaLab; User Id=sa; Password=123; TrustServerCertificate=True;";
+
+            using (SqlConnection conexion = new SqlConnection(cadena))
+            {
+                try
+                {
+                    conexion.Open();
+
+                    // 1. Le decimos de qué tabla sacamos los datos originalmente
+                    SqlDataAdapter adaptador = new SqlDataAdapter("SELECT * FROM Ciudadanos", conexion);
+
+                    // 2. MAGIA: Esto genera los comandos de guardar en automático
+                    SqlCommandBuilder constructor = new SqlCommandBuilder(adaptador);
+
+                    // 3. Rescatamos la tabla con los datos que tú ya editaste en la pantalla
+                    DataTable tablaEditada = (DataTable)dataGridView1.DataSource;
+
+                    // 4. Mandamos todos los cambios de golpe a SQL
+                    adaptador.Update(tablaEditada);
+
+                    MessageBox.Show("¡Cambios guardados con éxito en la base de datos!");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error al guardar: " + ex.Message);
+                }
+            }
+    }
+    }
+}
